d13db77c56e10e1673dd0a0c02fb4052f1897f9f
awkto <awkto@outlook.com>
2022-03-09 01:11:09 +0000

    added description

--- checktrace-domains-bulk.sh
+++ checktrace-domains-bulk.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#this script will show the NS records for each zone in the list,
+#  both the registrar NS records and the in-zone NS records will be shown
+
 domainslist=$1
 
 for domain in $(cat $domainslist);do \
